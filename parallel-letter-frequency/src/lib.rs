32167d4d4f224a781c066a5058b0fb5293354131
Zellyn Hunter <zellyn@gmail.com>
2021-03-21 11:04:26 -0400

    parallel-letter-frequency: feedback updates

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -1,55 +1,48 @@
+use crossbeam_utils::thread;
+use std::cmp;
 use std::collections::HashMap;
 use std::sync::mpsc;
-use std::thread;
-
-fn normalize(s: &str) -> String {
-    s.to_lowercase().chars().filter(|c| c.is_alphabetic()).collect()
-}
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    // Multiple publishers, single consumer means we can have one channel for the results…
+    let (size, remainder) = (input.len() / worker_count, input.len() % worker_count);
+    let actual_worker_count = cmp::min(worker_count, input.len());
+
+    // Multiple publishers, single consumer means we can have one channel for the results.
     let (result_tx, result_rx) = mpsc::channel();
-    // …but need one input channel per worker.
-    let mut channels: Vec<mpsc::Sender<String>> = Vec::with_capacity(worker_count);
 
-    // Spin up workers
-    for _ in 0..worker_count {
-        let result_tx = result_tx.clone();
-
-        let (input_tx, input_rx) = mpsc::channel();
-        channels.push(input_tx);
-        thread::spawn(move || {
-            let mut counts: HashMap<char, usize> = HashMap::new();
-
-            for s in input_rx {
-                for ch in s.chars() {
-                    let counter = counts.entry(ch).or_insert(0);
-                    *counter += 1;
+    thread::scope(|scope| {
+        let mut pos = 0;
+        for i in 0..actual_worker_count {
+            let chunk_size = if i < remainder { size + 1 } else { size };
+            let chunk = &input[pos..pos + chunk_size];
+            pos += chunk_size;
+            let result_tx = result_tx.clone();
+            scope.spawn(move |_| {
+                let mut counts: HashMap<char, usize> = HashMap::new();
+                for s in chunk {
+                    for ch in s
+                        .chars()
+                        .filter(|c| c.is_alphabetic())
+                        .map(|c| c.to_lowercase())
+                        .flatten()
+                    {
+                        *counts.entry(ch).or_insert(0) += 1;
+                    }
                 }
-            }
-            // When done
-            result_tx.send(counts).unwrap();
-        });
-    };
-
-    // Send in the inputs
-    // for chunk in input.into_iter().chunks(worker_count) {
-    //     chunk.iter().enumerate().for_each(|(i, &s)| {
-    //         channels[i].send(s).unwrap()
-    //     })
-    // }
-    for (i, &s) in input.iter().enumerate() {
-        channels[i % worker_count].send(normalize(s)).unwrap();
-    }
-
-    // Close all input channels
-    drop(channels);
+                result_tx.send(counts).unwrap();
+            });
+        }
+    })
+    .unwrap();
 
     // Merge the results
-    result_rx.iter().take(worker_count).fold(HashMap::new(), |mut ha, hb| {
-        for (ch, count) in hb {
-            *ha.entry(ch).or_insert(0) += count;
-        }
-        ha
-    })
+    result_rx
+        .iter()
+        .take(actual_worker_count)
+        .fold(HashMap::new(), |mut ha, hb| {
+            for (ch, count) in hb {
+                *ha.entry(ch).or_insert(0) += count;
+            }
+            ha
+        })
 }
